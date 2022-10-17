453ebc84994b3d3538574d42bd906dffd0a7dac0
MindaugasPaskevicius <106962850+MindaugasPaskevicius@users.noreply.github.com>
2022-10-17 20:58:59 +0300

    create bootstrap and cli-config files

---
+++ bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+$paths = array(__DIR__ . './src/models');
+$isDevMode = true;
+$proxyDir = null;
+$cache = null;
+$useSimpleAnnotationReader = false;
+
+$dbParams = array(
+    'driver' => 'pdo_mysql',
+    'host' => '127.0.0.1',
+    'dbname' => 'mini-cms',
+    'user' => 'root',
+    'password' => '',
+);
+
+$config = ORMSetup::createAnnotationMetadataConfiguration($paths, $isDevMode, $proxyDir, $cache,);
+$entityManager = EntityManager::create($dbParams, $config);
